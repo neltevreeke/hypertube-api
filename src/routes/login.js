ce4cfba14510e72632710c81e639496d8c557e74
neltevreeke <nelte.p.vreeke@gmail.com>
2020-04-14 13:26:31 +0200

    Got basic facebook log in flow working

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,18 @@
+const passport = require('passport')
+
 module.exports = app => {
   app.post('/login', async (req, res, next) => {
     // eslint-disable-next-line no-console
     console.log(req.body)
   })
+
+  app.get('/auth/facebook', passport.authenticate('facebook'))
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: 'http://localhost:3000/movies',
+    failureRedirect: 'http://localhost:3000/',
+    scopes: {}
+  }))
 
   return app
 }
---
+++ src/strategyKeys.js
@@ -0,0 +1,8 @@
+const FACEBOOK = {
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET
+}
+
+module.exports = {
+  FACEBOOK
+}
