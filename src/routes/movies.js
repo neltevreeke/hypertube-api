23a082fdcda7569253f9983f8e7c18c920de0d8e
neltevreeke <nelte.p.vreeke@gmail.com>
2020-09-14 14:32:39 +0200

    added movieDetails route, bettered yts queries

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -4,13 +4,29 @@ const fetch = require('node-fetch')
 module.exports = app => {
   app.get('/movies', authMiddleware, async (req, res, next) => {
     try {
-      fetch('https://yts.mx/api/v2/list_movies.json?limit=10&sort_by=rating&minimum_rating=8')
-        .then(response => response.json())
-        .then(response => {
-          res.json({
-            movies: response.data.movies
-          })
-        })
+      let response = await fetch('https://yts.mx/api/v2/list_movies.json?limit=10&sort_by=rating&minimum_rating=8')
+      response = await response.json()
+
+      res.json({
+        movies: response.data.movies
+      })
+    } catch (e) {
+      const error = new Error('not-found')
+      error.statusCode = 404
+      return next(error)
+    }
+  })
+
+  app.get('/movie-details/:id', authMiddleware, async (req, res, next) => {
+    const id = req.params.id
+
+    try {
+      let response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}&with_cast=true`)
+      response = await response.json()
+
+      res.json({
+        movieDetails: response.data.movie
+      })
     } catch (e) {
       const error = new Error('not-found')
       error.statusCode = 404
